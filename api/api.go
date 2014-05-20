8001cceca499a4770dc15701f2a14c93041859f0
Alex Suraci <asuraci@pivotallabs.com>
2014-05-19 17:51:07 -0700

    fill in dummy handlers for route healthy/unhealthy
    Signed-off-by: Eric Malm <emalm@pivotallabs.com>

--- api/api.go
+++ api/api.go
@@ -12,6 +12,9 @@ func NewServer(taskHandler, lrpHandler http.Handler) (http.Handler, error) {
 	handlers := map[string]http.Handler{
 		routes.TaskCompleted: taskHandler,
 		routes.LRPCompleted:  lrpHandler,
+
+		routes.RouteHealthy:   nil,
+		routes.RouteUnhealthy: nil,
 	}
 
 	return router.NewRouter(routes.Routes, handlers)
