a1f634061fca159a6318561864bcbac0fa3f3635
John Shahid <jvshahid@gmail.com>
2017-04-07 14:20:50 -0400

    wip
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- cmd/rep/config/config.go
+++ cmd/rep/config/config.go
@@ -13,7 +13,7 @@ import (
 	executorinit "code.cloudfoundry.org/executor/initializer"
 	"code.cloudfoundry.org/lager/lagerflags"
 	"code.cloudfoundry.org/locket"
-	"code.cloudfoundry.org/loggregator_v2"
+	"github.com/cloudfoundry-incubator/diego-loggregator/loggregator_v2"
 )
 
 type StackMap map[string]string
