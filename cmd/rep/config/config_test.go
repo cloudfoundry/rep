d4b5133c352ef9dce133893256fbff5e41f42470
Johanna Smith <josmith@pivotal.io>
2018-04-05 14:07:43 -0600

    update instance id in logging client to take string
    - enables use of guid as instance id
    - adheres to new go-loggregator client
    
    [#156379950]

--- cmd/rep/config/config_test.go
+++ cmd/rep/config/config_test.go
@@ -86,7 +86,7 @@ var _ = Describe("RepConfig", func() {
 				"loggregator_key_path": "key-path",
 				"loggregator_job_origin": "job-origin",
 				"loggregator_source_id": "my-source-id",
-				"loggregator_instance_id": 1
+				"loggregator_instance_id": "my-instance-id"
 			},
 			"max_cache_size_in_bytes": 101,
 			"max_concurrent_downloads": 11,
@@ -234,7 +234,7 @@ var _ = Describe("RepConfig", func() {
 				KeyPath:    "key-path",
 				JobOrigin:  "job-origin",
 				SourceID:   "my-source-id",
-				InstanceID: 1,
+				InstanceID: "my-instance-id",
 			},
 		}))
 	})
